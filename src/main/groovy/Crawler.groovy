8423333bb5bb975cacd961ac9051ac5b95e50b13
Gabriel Holanda de Freitas <gabriel.holanda@polealimentos.com.br>
2025-10-17 17:20:24 -0300

    task 1 - acessando página por página

--- src/main/groovy/Crawler.groovy
+++ src/main/groovy/Crawler.groovy
@@ -2,12 +2,28 @@ import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 
-def url = 'https://pt.wikipedia.org/wiki/Wikip%C3%A9dia:P%C3%A1gina_principal'
+def url = 'https://www.gov.br/ans/pt-br'
 
 Document document = Jsoup.connect(url).get()
+Element linkEspacoPrestador = document.selectFirst("a:contains(Espaço do Prestador)")
+String linkEspacoPrestadorAbsoluto  = linkEspacoPrestador.attr("abs:href")
+String textoLinkEspacoPrestador = linkEspacoPrestador.text()
 
-Element titulo = document.selectFirst('h1')
+Document documentEspacoPrestador = Jsoup.connect(linkEspacoPrestadorAbsoluto).get()
+Element linkTISS = documentEspacoPrestador.selectFirst("a:contains(TISS - Padrão)")
+String linkTISSAbsoluto = linkTISS.attr("abs:href")
+String textoLinkTISS = linkTISS.text() 
 
-String textoTitulo = titulo.text()
+Document documentPadraoTISS = Jsoup.connect(linkTISSAbsoluto).get()
+Element PadraoLinkTISS = documentPadraoTISS.selectFirst("a:contains(Clique aqui para acessar)")
+String PadraoLinkTISSAbsoluto = PadraoLinkTISS.attr("abs:href")
+String textoPadraoLinkTISS = PadraoLinkTISS.text()
 
-println "Título da página da Wikipedia: ${textoTitulo}"
+println "$textoLinkEspacoPrestador" 
+println "$linkEspacoPrestadorAbsoluto" 
+
+println "$textoLinkTISS"
+println "$linkTISSAbsoluto"
+
+println "$textoPadraoLinkTISS"
+println "$PadraoLinkTISSAbsoluto"
